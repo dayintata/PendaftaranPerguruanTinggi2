95d3fc730fd3f06fbb80bf3288add18e46d0c9e0
Dayinta-PC <dayintaindahsari@gmail.com>
2016-09-11 18:40:16 +0700

    text view hasil

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3006/pendaftaranperguruantinggi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3006/pendaftaranperguruantinggi/MainActivity.java
@@ -2,12 +2,23 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3006.pendaftaranperguruantinggi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etNama;
+
+    Spinner spKelulusan;
+    TextView tvHasil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spKelulusan = (Spinner)findViewById(R.id.spinnerkelulusan);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
     }
 }
